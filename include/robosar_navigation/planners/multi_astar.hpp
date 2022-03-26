92091a9f655c02836f9579bd201cc32e79e37cca
Charvi <charvigupta96@gmail.com>
2022-03-26 14:17:38 -0400

    Multi astar added.. yet to integrate with task allocator

---
+++ include/robosar_navigation/planners/multi_astar.hpp
@@ -0,0 +1,47 @@
+#ifndef MULTI_ASTAR_HPP
+#define MULTI_ASTAR_HPP
+
+#include <vector>
+#include <ros/ros.h>
+using namespace std;
+
+class MultiAStar {
+
+public:
+    MultiAStar(Graph* graph, vector<pair<double,double>> currPos, vector<pair<double,double>> targetPos, int numAgents):pg(graph){
+        this->numAgents = numAgents;
+        for(int i=0;i<numAgents;i++)
+            agentId.push_back(i);
+        this->curr_Pos = currPos;
+        this->tar_Pos = targetPos;
+        //curr_Pos.push_back(make_pair(0,0));
+        //tar_Pos.push_back(make_pair(100,100));
+    }
+
+    ~MultiAStar(){
+    }
+
+    bool run_multi_astar(){
+        assignPriority(agentId);
+        for(int i=0;i<numAgents;i++){
+            int index = agentId[i];
+            double start[2]={curr_Pos[index].first,curr_Pos[index].second};
+            double goal[2] = {tar_Pos[index].first,tar_Pos[index].second};
+            AStar planner(pg,goal,start);
+            bool path_for_agent = planner.run_planner(100); //TODO check what format path is returned in
+        }
+        return false;
+    }
+
+private:
+    int numAgents;
+    Graph* pg;
+    vector<int> agentId;
+    vector<pair<double,double>> curr_Pos;
+    vector<pair<double,double>> tar_Pos;
+    vector<int> assignPriority(vector<int> agentId){
+        return agentId;
+    }
+};
+
+#endif
--- src/robosar_navigation_node.cpp
+++ src/robosar_navigation_node.cpp
@@ -3,17 +3,45 @@
 #include <ros/ros.h>
 #include "graph_2d_grid.hpp"
 #include "astar.hpp"
-
+#include "multi_astar.hpp"
+#include "string.h"
+#include <tf/transform_listener.h>
+#include <tf2/buffer_core.h>
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "robosar_navigation_node");
-
   //move_base::MoveBase move_base( buffer );
   Graph2DGrid gridmap;
-
+  int numAgents=3;
   double goal[] = {0.0,0.0};
   double start[] = {0.0,0.0};
-  AStar planner(&gridmap,goal,start);
+  tf2::BufferCore buffer_core;
+  std::vector<std::pair<double,double>> currPos;
+  std::vector<std::pair<double,double>> targetPos;
+  tf::TransformListener listener;
+  ros::Duration(5.0).sleep();
+  tf::StampedTransform transform[numAgents];
+  try{
+    for(int i=0;i<numAgents;i++){
+      std::string target_base = "robot_"+std::to_string(i)+"/base_link";
+      listener.lookupTransform("map", target_base,
+                             ros::Time(0), transform[i]);
+      currPos.push_back(std::make_pair(transform[i].getOrigin().x(),transform[i].getOrigin().y()));
+    }
+    ROS_INFO("Transform0 x: %f y:%f",transform[0].getOrigin().x(),transform[0].getOrigin().y());
+    ROS_INFO("Transform1 x: %f y:%f",transform[1].getOrigin().x(),transform[1].getOrigin().y());
+    ROS_INFO("Transform2 x: %f y:%f",transform[2].getOrigin().x(),transform[2].getOrigin().y());
+  }
+  catch (tf::TransformException ex){
+    ROS_ERROR("%s",ex.what());
+    ros::Duration(1.0).sleep();
+  }
+  targetPos.push_back(std::make_pair(10,10));
+  targetPos.push_back(std::make_pair(10,10));
+  targetPos.push_back(std::make_pair(10,10));
+  MultiAStar multi_astar(&gridmap,currPos,targetPos,numAgents);
+  bool status = multi_astar.run_multi_astar();
+  //AStar planner(&gridmap,goal,start);
 
   //ros::MultiThreadedSpinner s;
   ros::spin();
