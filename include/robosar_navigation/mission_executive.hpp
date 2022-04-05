c9dc8f83ae80cc37e638533ac9640e85928398be
Neel <neilpatilcoldplay@gmail.com>
2022-04-05 19:18:33 -0400

    Added new service client

--- include/robosar_navigation/mission_executive.hpp
+++ include/robosar_navigation/mission_executive.hpp
@@ -7,6 +7,7 @@
 #include "multi_astar.hpp"
 #include <ros/console.h>
 #include <std_msgs/Bool.h>
+#include "robosar_messages/agent_status.h"
 
 class MissionExecutive {
 
@@ -15,9 +16,11 @@ public:
 
         ROS_INFO("Starting a new RoboSAR Nav Mission! Get ready for a show!");
 
-        status_subscriber_ = nh_.subscribe("cmd_vel", 1, &MissionExecutive::statusCallback, this);
+        status_subscriber_ = nh_.subscribe("/robosar_agent_bringup/status", 1, &MissionExecutive::statusCallback, this);
         // Get latest fleet info from agent bringup
-
+        status_client = nh_.serviceClient<robosar_messages::agent_status>("fleet_status");
+        
+        fleet_status_outdated = false;
     }
 
     ~MissionExecutive() {
@@ -28,11 +31,14 @@ public:
 private:
 
     void statusCallback(const std_msgs::Bool &status_msg) {
-
+        fleet_status_outdated = true;
     }
+
+    ros::ServiceClient status_client; 
     Graph3DGrid gridmap;
     ros::Subscriber status_subscriber_;
     ros::NodeHandle nh_;
+    bool fleet_status_outdated;
 };
 
 #endif
