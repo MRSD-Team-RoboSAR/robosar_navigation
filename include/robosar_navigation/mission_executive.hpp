6a00f6b50978327ddd80e6206540c9983154fee5
Neel <neilpatilcoldplay@gmail.com>
2022-04-05 19:11:19 -0400

    Added new subscriber for agent status messages

--- include/robosar_navigation/mission_executive.hpp
+++ include/robosar_navigation/mission_executive.hpp
@@ -5,14 +5,34 @@
 
 #include "graph_3d_grid.hpp"
 #include "multi_astar.hpp"
+#include <ros/console.h>
+#include <std_msgs/Bool.h>
 
 class MissionExecutive {
 
-    public:
+public:
+    MissionExecutive() : nh_("") {
 
+        ROS_INFO("Starting a new RoboSAR Nav Mission! Get ready for a show!");
 
-    private:
+        status_subscriber_ = nh_.subscribe("cmd_vel", 1, &MissionExecutive::statusCallback, this);
+        // Get latest fleet info from agent bringup
 
+    }
+
+    ~MissionExecutive() {
+
+    }
+        
+
+private:
+
+    void statusCallback(const std_msgs::Bool &status_msg) {
+
+    }
+    Graph3DGrid gridmap;
+    ros::Subscriber status_subscriber_;
+    ros::NodeHandle nh_;
 };
 
 #endif
