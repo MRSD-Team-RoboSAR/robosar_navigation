047a978355e5f864016b2e0308b12b909d599598
Neel <neilpatilcoldplay@gmail.com>
2022-04-05 18:55:45 -0400

    Added new mission executive class

---
+++ include/robosar_navigation/mission_executive.hpp
@@ -0,0 +1,18 @@
+// Created by Indraneel and Charvi on 5/04/21
+
+#ifndef MISSION_EXECUTIVE_HPP
+#define MISSION_EXECUTIVE_HPP
+
+#include "graph_3d_grid.hpp"
+#include "multi_astar.hpp"
+
+class MissionExecutive {
+
+    public:
+
+
+    private:
+
+};
+
+#endif
